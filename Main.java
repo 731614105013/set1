fb14d421386b7b0565f06ba1284363b2f2302dad
731614105013 <gomathiammupk29@gmail.com>
2017-07-04 17:38:01 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,22 @@
+# set1
+import java.io.*;
+import java.util.*;
+public class Main{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+if(n>0)
+{
+System.out.println("n is positive");
+}
+else if(n<0)
+{
+System.out.println("n is negative");
+}
+else
+{
+System.out.println("n is zero");
+}
+}
+}
